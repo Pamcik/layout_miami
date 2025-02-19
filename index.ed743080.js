2302b1656791756555d60142500f24c530020dfe
Pamela Szczerbiak <pamela.szczerbiak@gmail.com>
2025-02-19 11:01:17 +0100

    make build

---
+++ index.ed743080.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")}),document.querySelector("#form").addEventListener("submit",function(e){e.preventDefault(),this.reset()});
+//# sourceMappingURL=index.ed743080.js.map
